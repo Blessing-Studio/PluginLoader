6a0bb67ab7afe1da0a7462d803350915f5fa1715
JWJUN233233 <1306334428@qq.com>
2023-09-02 08:48:09 +0800

    添加测试项目

---
+++ src/PluginLoader.Test/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+public static class Test
+{
+    public static string var1 = "hello";
+    public static void Main(string[] args)
+    {
+        wonderlab.PluginLoader.PluginLoader.LoadAll();
+        Console.WriteLine(var1);
+    }
+}
