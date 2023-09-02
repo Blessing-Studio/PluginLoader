a87a2450ffef0cdf5e1e8fed11d58ee6252e9df3
JWJUN233233 <1306334428@qq.com>
2023-09-02 09:52:12 +0800

    添加测试插件

--- src/PluginLoader.Test/Program.cs
+++ src/PluginLoader.Test/Program.cs
@@ -6,6 +6,7 @@ public static class Test
     public static void Main(string[] args)
     {
         wonderlab.PluginLoader.PluginLoader.LoadAll();
+        wonderlab.PluginLoader.PluginLoader.EnableAll();
         Console.WriteLine(var1);
     }
 }
---
+++ src/TestPlugin/Class1.cs
@@ -0,0 +1,30 @@
+﻿using wonderlab.PluginLoader.Attributes;
+using wonderlab.PluginLoader.Interfaces;
+
+namespace TestPlugin
+{
+    [Plugin("TestPlugin", "测试插件", "1.0.0", "{A93D168A-C410-48C3-92E8-86375503DBD3}", "Ddggdd135")]
+    public class TestPlugin : IPlugin
+    {
+        public void OnDisable()
+        {
+            Test.var1 = "hello";
+        }
+
+        public bool OnEnable()
+        {
+            Test.var1 = "hello world";
+            return true;
+        }
+
+        public void OnLoad()
+        {
+            Console.WriteLine("测试插件已加载");
+        }
+
+        public void OnUnload()
+        {
+
+        }
+    }
+}
